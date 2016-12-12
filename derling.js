cc209e7996859f9790fd6ef92dee21d914a55e1a
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2016-12-12 17:56:58 +0100

    Update to d3.v4, add another rotation mode

--- derling.js
+++ derling.js
@@ -4,13 +4,29 @@ d3.derling = function() {
         return Math.sqrt(3) / 2 * a
     };
 
-    var equilateralTriangle = function(a){
+    var equilateralTriangle = function(a, flipped){
         var h = getHeight(a);
-        return 'M 0 ' + h + ' L ' + (a / 2) +' '+ 0 + ' L ' + a + ' ' + h + ' z';
-    };
-    var equilateralTriangleFlipped = function(a){
-        var h = getHeight(a);
-        return 'M 0  0 L' + (a / 2) + ' ' + h + ' L ' + a +' 0 z';
+        rot = rotation;
+        if (flipped) {
+          if (rotation % 2) {
+            rot = rot - 1;
+          } else {
+            rot = rot + 1;
+          }
+        }
+        if (rot === 0) {
+          // /\
+          return 'M 0 ' + h + ' L ' + (a / 2) +' 0 L ' + a + ' ' + h + ' z';
+        } else if (rot === 1) {
+          // \/
+          return 'M 0 0 L' + (a / 2) + ' ' + h + ' L ' + a + ' 0 z';
+        } else if (rot === 2) {
+          // >
+          return 'M 0 0 L ' + h + ' ' + (a / 2) + ' L 0 ' + a + ' z';
+        } else if (rot === 3) {
+          // <
+          return 'M ' + h + ' 0 L' + h + ' ' + a + ' L 0 ' + (a / 2) + ' z';
+        }
     };
 
     var getSideFromArea = function(A) {
@@ -18,16 +34,18 @@ d3.derling = function() {
     };
 
     var isFlipped = function(d) {
-        return (d.x + d.y) % 2 == !flipped;
+      return !((d.x + d.y) % 2);
     };
 
-    var flipped = true;
+    var rotation = 0;
     var paddingX = 3;
     var paddingY = 3;
-    var showLabel = true;
+    var showLabel = false;
     var sideWidth = 10;
     var shapeFill;
     var textFill;
+    var offsets = {};
+    var scale;
 
 
     // h = √3 / 2 * a
@@ -46,19 +64,38 @@ d3.derling = function() {
             var g = element.append('g')
                 .attr('transform', function(d){
                     var a = sideWidth;
-                    var h = getHeight(a);
-                    var t = 'translate(' + d.x * ((a / 2) + paddingX) + ', ' + d.y * (h + paddingY) + ')';
+                    if (scale !== undefined) {
+                      a = getSideFromArea(scale(d.value) * 100);
+                    }
+                    var h = getHeight(a), x, y;
+                    if (rotation < 2) {
+                      x = d.x * ((a / 2) + paddingX);
+                      y = d.y * (h + paddingY);
+                    } else {
+                      x = d.x * (h + paddingX);
+                      y = d.y * ((a / 2) + paddingY);
+                    }
+                    if (scale !== undefined) {
+                      var min = scale.range()[0];
+                      var offset = offsets[d.x + '@' + d.y];
+                      if (offset !== undefined) {
+                        x += offset[0];
+                        y += offset[1];
+                      }
+                      offsets[(d.x + 1) + '@' + d.y] = [offset[0] + (a - min), offset[1] + (a - min)];
+                    }
+                    var t = 'translate(' + x + ', ' + y + ')';
                     return t;
 
                 });
 
             g.append('path')
-                .attr('d', function(d) { 
+                .attr('d', function(d) {
                     var a = sideWidth;
-                    if (isFlipped(d)) {
-                        return equilateralTriangleFlipped(a);
+                    if (scale !== undefined) {
+                      a = getSideFromArea(scale(d.value) * 100);
                     }
-                    return equilateralTriangle(a);
+                    return equilateralTriangle(a, isFlipped(d));
                 })
                 .style('fill', function(d){
                     if (typeof shapeFill === "function") {
@@ -71,9 +108,15 @@ d3.derling = function() {
                 g.append('text')
                     .attr('transform', function(d){
                         var a = sideWidth;
-                        var h = getHeight(a);
-                        var y = isFlipped(d) ? h / 2.5 : h / 1.2; 
-                        return 'translate( ' + a / 2 + ', ' + y + ')';
+                        var h = getHeight(a), x, y;
+                        if (rotation < 2) {
+                          x = a / 2;
+                          y = (isFlipped(d) + (rotation % 2)) % 2 ? h / 2.5 : h / 1.2;
+                        } else {
+                          y = a / 1.8
+                          x = (isFlipped(d) + (rotation % 2)) % 2 ? h / 1.9 : h / 2.5;
+                        }
+                        return 'translate( ' + x + ', ' + y + ')';
                     })
                     .attr('text-anchor', 'middle')
                     .text(function(d){ return d.label; })
@@ -90,9 +133,9 @@ d3.derling = function() {
     }
 
     // getter / setter for the label property
-    my.flipped = function(value) {
-        if (!arguments.length) return flipped;
-        flipped = value;
+    my.rotation = function(value) {
+        if (!arguments.length) return rotation;
+        rotation = value;
         return my;
     };
     my.sideWidth = function(value) {
@@ -110,6 +153,11 @@ d3.derling = function() {
         shapeFill = value;
         return my;
     };
+    my.scale = function(value) {
+        if (!arguments.length) return scale;
+        scale = value;
+        return my;
+    };
     my.textFill = function(value) {
         if (!arguments.length) return textFill;
         textFill = value;
@@ -117,4 +165,4 @@ d3.derling = function() {
     };
 
     return my;
-}
+};
